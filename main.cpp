f62c632a622e930ed4f061b5c510d2d4c3a1b390
s4ncheese <alex_rusanov03@mail.ru>
2022-08-07 12:49:01 +0300

    small playing field added

--- main.cpp
+++ main.cpp
@@ -4,24 +4,81 @@
 #include <windows.h>
 #include <ctime>
 #include <vector>
+#include <algorithm>
+
+#define MIN_SIZE 21
 
 struct Playing_field {
 
-    Playing_field(unsigned size) {
-        std::vector<std::vector<char>> _field;
+    Playing_field(unsigned n_rows, unsigned n_cols) {
+        _set_size(n_rows, n_cols);
 
+        _field = (char **) malloc(sizeof(char *) * _n_rows);
+        for (unsigned i_rows = 0; i_rows < _n_rows; i_rows++)
+            _field[i_rows] = (char *) malloc(sizeof(char) * _n_cols);
+
+        for (unsigned i_row = 0; i_row < _n_rows; i_row++)
+            for (unsigned i_col = 0; i_col < _n_cols; i_col++)
+                _field[i_row][i_col] = '.';
     }
 
-    unsigned _get_size() {
-        return _size;
+    Playing_field() {
+        unsigned n_rows = 20, n_cols = 20;
+        _set_size(n_rows, n_cols);
+
+        _field = (char **) malloc(sizeof(char *) * _n_rows);
+        for (unsigned i_rows = 0; i_rows < _n_rows; i_rows++)
+            _field[i_rows] = (char *) malloc(sizeof(char) * _n_cols);
+
+        for (unsigned i_row = 0; i_row < _n_rows; i_row++)
+            for (unsigned i_col = 0; i_col < _n_cols; i_col++)
+                _field[i_row][i_col] = '.';
+    }
+
+    void _set_size(unsigned n_rows, unsigned n_cols) {
+        _n_rows = n_rows;
+        _n_cols = n_cols;
+    }
+
+    unsigned _get_rows(){
+        return _n_rows;
+    }
+
+    unsigned _get_cols(){
+        return _n_cols;
     }
 
     void print_field() {
+        unsigned n_rows = _get_rows();
+        unsigned n_cols = _get_cols();
 
+        for (unsigned i_col = 0; i_col < _n_cols + 1; i_col++) {
+            if (!i_col) {
+                std::cout << 0 << "  ";
+            } else {
+                std::cout << 0 + i_col << ' ';
+            }
+        }
+        std::cout << '\n';
+
+        for (unsigned i_row = 0; i_row < n_rows; i_row++) {
+            if (i_row < 9) {
+                std::cout << 1 + i_row << ' ' << ' ';
+            } else {
+                std::cout << 1 + i_row << ' ';
+            }
+            for (unsigned i_col = 0; i_col < n_cols; i_col++)
+                if (i_col < 9) {
+                    std::cout << _field[i_row][i_col] << ' ';
+                } else {
+                    std::cout << ' ' << _field[i_row][i_col] << ' ';
+                }
+            std::cout << '\n';
+        }
     }
 
 private:
-    unsigned _size;
+    unsigned _n_rows, _n_cols;
     char **_field;
 public:
 };
@@ -102,16 +159,36 @@ int main() {
             std::cin >> size;
 
             while (size < 1 || size > 3) {
-                std::cerr << "Неверное значение!!! Попробуйте ещё раз >>> \n";
+                std::cerr << "Неверное значение!!! Попробуйте ещё раз >>>\n";
                 std::cin >> size;
             }
 
-            Playing_field f(size);
-            f.print_field();
+            Playing_field field;
+            switch (size) {
+                case 1: {
+                    field._set_size(20, 20);
+                    break;
+                }
+                case 2: {
+                    field._set_size(30, 30);
+                    break;
+                }
+                case 3: {
+                    field._set_size(40, 40);
+                }
+            }
 
             std::cout << "Поле создано. Начать игру? \n"
                          "1. Да\n"
                          "2. Нет\n";
+
+            std::cin >> user_change;
+            while (user_change != 1 && user_change != 2) {
+                std::cerr << "Неверное значение!!! Попробуйте ещё раз >>>\n";
+                std::cin >> user_change;
+            }
+
+            field.print_field();
         }
     }
 
